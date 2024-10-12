09beb16dc2d551dbd78893866c059f41999e6be4
Hitesh Saini <hitesh9694saini@gmail.com>
2024-10-12 12:10:05 +0530

    push

--- app/contextapi/url.js
+++ app/contextapi/url.js
@@ -1,5 +1,6 @@
  const Url = {
-    base_url: 'http://localhost:8000',
+    // base_url: 'http://localhost:8000',
+    base_url: 'https://cityviewbackend.onrender.com',
 
     create_post: '/api/v1/posts',
     get_posts: '/api/v1/posts',
